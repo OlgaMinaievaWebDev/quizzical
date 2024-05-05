b080cb648162642fbf24d3a80b0e43eac1d7fbd1
Olga <minaeva9@gmail.com>
2024-05-05 17:34:53 -0400

    fetch data.results

--- src/components/Ouiz.jsx
+++ src/components/Ouiz.jsx
@@ -1,3 +1,24 @@
+import { useEffect } from "react";
+
 export default function Quiz() {
-  return <div>Lets do it</div>;
+  //fetch data
+  useEffect(function () {
+    async function fetchQuestions() {
+      try {
+        const res = await fetch(
+          "https://opentdb.com/api.php?amount=5&type=multiple"
+        );
+        const data = await res.json();
+        console.log(data.results);
+      } catch (error) {
+        console.log("Error fetching quiz data", error);
+      }
+    }
+    fetchQuestions();
+  }, []);
+  return (
+    <div>
+      <h1>Quizzical questions</h1>
+    </div>
+  );
 }
