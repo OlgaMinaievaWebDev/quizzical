7fd34d34684ab0221160d330d1f242eb160612df
Olga <minaeva9@gmail.com>
2024-05-05 17:14:29 -0400

    first init

---
+++ src/components/Start.jsx
@@ -0,0 +1,11 @@
+import "./Start.css";
+
+export default function Start() {
+  return (
+    <div className="start-page">
+      <h1>Quizzical</h1>
+      <p>Welcome! Test your knowledge with this quiz!</p>
+      <button className="start-btn">Start Quiz</button>
+    </div>
+  );
+}
